dc54b0832bbdbcb55cfaa7c1943e613c0bd02e5c
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 14:10:31 +0700

    tambah rute kartu peminjaman,ketegori,peminjam,peminjaman,penerbit,petugas

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', [PageController::class,'home']);
 
 Route::get('/data/buku', [DataController::class,'ms_buku']);
 Route::get('/data/detail_pinjam', [DataController::class,'ms_detail_pinjam']);
+Route::get('/data/kartu_pendaftaran', [DataController::class,'ms_kartu_pendaftaran']);
+Route::get('/data/kategori', [DataController::class,'ms_kategori']);
+Route::get('/data/peminjam', [DataController::class,'ms_peminjam']);
+Route::get('/data/peminjaman', [DataController::class,'ms_peminjaman']);
+Route::get('/data/penerbit', [DataController::class,'ms_penerbit']);
+Route::get('/data/petugas', [DataController::class,'ms_petugas']);
