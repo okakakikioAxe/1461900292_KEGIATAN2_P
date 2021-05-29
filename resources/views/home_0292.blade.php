3dd262690380a9fcf0adbea058af36cadf2c5399
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 11:53:03 +0700

    tambah file home

---
+++ resources/views/home_0292.blade.php
@@ -0,0 +1,28 @@
+@extends('template.main_0292')
+
+@section('judul','home')
+
+@section('konten')
+    <br>
+    <div class="container">
+        <h2>Selamat Datang</h2>
+    </div>
+    <div class="container text-center">
+        <br>
+        <br>
+        <h6>Tampilkan sigle data</h6>
+        <a href="">
+            <button type="button" class="btn btn-primary" style="width: 120px;height: auto;">Single Data</button>
+        </a>
+        <br>
+        <br>
+        <br>
+        <h6>atau</h6>
+        <br>
+        <br>
+        <h6>Tampilkan multiple data</h6>
+        <a href="">
+            <button type="button" class="btn btn-primary" style="width: 120px;height: auto;">Multiple Data</button>
+        </a>
+    </div>
+@endsection
