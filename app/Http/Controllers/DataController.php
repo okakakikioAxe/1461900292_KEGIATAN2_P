cd7fdf7c1e43b575ed7afa8f837af5ae9af94264
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 13:38:25 +0700

    edit fungsi ms buku

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -11,7 +11,7 @@ class DataController extends Controller
         $buku = DB::table('ms_buku')->get();
         $header = ['kd buku','kd kategori','kd penerbit','judul','jumlah','pengarang','thn terbit'];
        
-        return view('buku_0292',['header'=>$header,'data'=>$buku]);
+        return view('singleData_0292',['header'=>$header,'data'=>$buku, 'judul' => $judul_list, 'search'=>$judul_search, 'tabel'=> $nama_tabel, 'field'=>$nama_field]);
     }
 
     public function ms_detail_pinjam(){
