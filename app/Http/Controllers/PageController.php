44de14e0cfb56e1dbb4e24c4d640133444313bfd
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 12:59:00 +0700

    hapus fungsi data

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,13 +10,4 @@ class PageController extends Controller
     public function home(){
         return view('home_0292');
     }
-
-    public function data(){
-       $buku = DB::table('ms_buku')->get();
-       $header = ['kd buku','kd kategori','kd penerbit','judul','jumlah','pengarang','thn terbit'];
-       $nama_tabel = ['ms_buku'];
-       $judul_list=['list buku'];
-       $judul_search=['masukkan kode buku'];
-       return view('singleData_0292',['header'=>$header,'data'=>$buku, 'judul' => $judul_list, 'search'=>$judul_search, 'tabel'=> $nama_tabel]);
-    }
 }
