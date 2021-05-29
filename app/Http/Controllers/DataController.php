727a0761b734307c07acf444e2eb35a3049bd7c3
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 12:55:07 +0700

    tambah fungsi ms_buku

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
-    //
+    public function ms_buku(){
+        $buku = DB::table('ms_buku')->get();
+       $header = ['kd buku','kd kategori','kd penerbit','judul','jumlah','pengarang','thn terbit'];
+       $nama_tabel = ['ms_buku'];
+       $judul_list=['list buku'];
+       $judul_search=['masukkan kode buku'];
+       return view('singleData_0292',['header'=>$header,'data'=>$buku, 'judul' => $judul_list, 'search'=>$judul_search, 'tabel'=> $nama_tabel]);
+    }
 }
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,8 +14,9 @@ class PageController extends Controller
     public function data(){
        $buku = DB::table('ms_buku')->get();
        $header = ['kd buku','kd kategori','kd penerbit','judul','jumlah','pengarang','thn terbit'];
+       $nama_tabel = ['ms_buku'];
        $judul_list=['list buku'];
        $judul_search=['masukkan kode buku'];
-       return view('singleData_0292',['header'=>$header,'data'=>$buku, 'judul' => $judul_list, 'search'=>$judul_search]);
+       return view('singleData_0292',['header'=>$header,'data'=>$buku, 'judul' => $judul_list, 'search'=>$judul_search, 'tabel'=> $nama_tabel]);
     }
 }
