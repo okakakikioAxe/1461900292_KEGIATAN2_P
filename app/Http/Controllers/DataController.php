a48ff664c559a1d9a5171de7fe241360ece8a683
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 13:07:07 +0700

    tambah fungsi ms_detail_pinjam

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -15,4 +15,13 @@ class DataController extends Controller
        $judul_search=['masukkan kode buku'];
        return view('singleData_0292',['header'=>$header,'data'=>$buku, 'judul' => $judul_list, 'search'=>$judul_search, 'tabel'=> $nama_tabel]);
     }
+
+    public function ms_detail_pinjam(){
+        $data = DB::table('ms_detail_pinjam')->get();
+       $header = ['kd peminjaman','kd buku','tgl kembali','denda','status kembali'];
+       $nama_tabel = ['ms_detail_pinjam'];
+       $judul_list=['list peminjaman'];
+       $judul_search=['masukkan kode peminjaman'];
+       return view('singleData_0292',['header'=>$header,'data'=>$data, 'judul' => $judul_list, 'search'=>$judul_search, 'tabel'=> $nama_tabel]);
+    }
 }
