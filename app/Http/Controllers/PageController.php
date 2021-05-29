d6c9aeadab50d3bfbac5d0ab4850d339e4fffe92
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-29 12:14:19 +0700

    tambah fungsi redirect ke laman single data

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function home(){
         return view('home_0292');
+    }
+
+    public function data(){
+       $buku = DB::table('ms_buku')->get();
+       return view('singleData_0292',['$data'=>$buku]);
     }
 }
